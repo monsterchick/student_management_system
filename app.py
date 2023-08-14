999a8d127e206420f72492ac8b2bc8e4ef6f97b3
kwan <kwanho.app@gmail.com>
2023-08-14 20:13:46 +0100

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,10 @@ def Welcome():  # put application's code here
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run()
