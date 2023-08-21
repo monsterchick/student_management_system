d64f800cafc572cd12c58bfe763dbc8a05a343c3
kwan <kwanho.app@gmail.com>
2023-08-21 13:52:25 +0100

    added fake student data

--- attach/mysql_codes.sql
+++ attach/mysql_codes.sql
@@ -1,3 +1,4 @@
+# table tbl_manager
 SHOW DATABASES;
 USE mydb;
 SHOW TABLES;	# show table list
@@ -48,3 +49,37 @@ ALTER TABLE tbl_manager MODIFY COLUMN mUsername VARCHAR(12);    # change VARCHAR
 
 SELECT * FROM tbl_manager WHERE mId=10;     # search row which's mId=10
 SELECT * FROM tbl_manager WHERE mUsername='asdf' AND mPassword=222;     # search row which's mUsername='asdf' and mPassword=222
+
+# table tbl_stu_info
+CREATE TABLE tbl_stu_info (
+	sId INT NOT NULL AUTO_INCREMENT,
+	sName VARCHAR(25),
+    sAge INT,
+    sMobile VARCHAR(20),
+    sCourse VARCHAR(50),
+	PRIMARY KEY (sId)
+);
+
+SELECT * FROM tbl_stu_info;
+
+ALTER TABLE tbl_stu_info CHANGE sNAME sFirstName VARCHAR(25);
+ALTER TABLE tbl_stu_info ADD sLastName VARCHAR(25) AFTER sFirstName;    # add the new column after sFirstName
+ALTER TABLE tbl_stu_info CHANGE sCourse sAddress VARCHAR(100);
+ALTER TABLE tbl_stu_info MODIFY sId INT;
+INSERT INTO tbl_stu_info(
+sId,
+sFirstName,
+sLASTNAME,
+sAge,
+sMobile,
+sAddress
+)
+VALUES (
+0,
+'Jimmy',
+'Smith',
+18,
+'+0 1234567890',
+'286 Wimborne Rd, Talbot Woods, Bournemouth BH3 7AT, United Kingdom'
+);
+DELETE FROM tbl_stu_info WHERE sId=0;
---
+++ attach/temp_bk.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, redirect, request
+from database_operator import Operator
+
+
+
+app = Flask(__name__)
+@app.route('/')
+def Welcome():  # put application's code here
+    return render_template('welcome.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    operator = Operator()
+    if request.method=='POST':
+        # information for registering account
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(username, password)
+        info_check = operator.login_verification(inp_usr=username, inp_psw=password)
+        print(info_check)
+        if info_check == True:
+            return redirect('/home?user={}'.format(username))
+        else:
+            print('fail to log in. Please check your username or password.')
+            prompt = 'Fail to log in. Try again!'
+            return render_template('login.html', prompt=prompt)
+
+
+
+    return render_template('login.html')
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    operator = Operator()
+    if request.method=='POST':
+        # while True:
+        # information for registering account
+        username = request.form.get('username')
+        password = request.form.get('password')
+        verification = request.form.get('verification')
+        email = request.form.get('email')
+        # print(username, email, password, verification)
+
+        # sign up successfully
+        if password == verification:
+            operator.add(inp_usr=username, inp_psw=password, inp_email=email)
+            return redirect('/login')
+        else:
+            # sign up fail
+            print('something wrong! try again.')
+
+    return render_template('signup.html')
+
+@app.route('/home')
+def home():
+    operator = Operator()
+    user = request.args.get('user')
+    # print(user)
+
+    students = operator.show_student_list()
+    print(students)
+
+    return render_template('manage_panel.html', user=user, students=students)
+
+@app.route('/forgot')
+def forgot():
+    return render_template('forgot.html')
+
+if __name__ == '__main__':
+    app.run()
