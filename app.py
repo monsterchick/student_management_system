8d689a4e26e4197247645da233eae7a509eb44df
kwan <kwanho.app@gmail.com>
2023-09-09 16:38:11 +0100

    home page left

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, redirect, request
 from database_operator import Operator
 
-
-
 app = Flask(__name__)
+
+
 @app.route('/')
-def Welcome():  # put application's code here
+def Welcome():
+    '''
+    welcome page
+    '''
     return render_template('welcome.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    '''
+    This checks if the input entered by user verifies manager's database
+    '''
     operator = Operator()
-    if request.method=='POST':
+    if request.method == 'POST':
         # information for registering account
         username = request.form.get('username')
         password = request.form.get('password')
@@ -25,25 +32,27 @@ def login():
             prompt = 'Fail to log in. Try again!'
             return render_template('login.html', prompt=prompt)
 
-
-
     return render_template('login.html')
 
-@app.route('/signup',methods=['GET','POST'])
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    '''
+    manager sign up page
+    '''
     operator = Operator()
-    if request.method=='POST':
+    if request.method == 'POST':
         # while True:
         # information for registering account
         username = request.form.get('username')
         password = request.form.get('password')
         verification = request.form.get('verification')
         email = request.form.get('email')
-        # print(username, email, password, verification)
+        print(username, email, password, verification)
 
         # sign up successfully
         if password == verification:
-            operator.add(inp_usr=username, inp_psw=password, inp_email=email)
+            operator.sign_up(inp_usr=username, inp_psw=password, inp_email=email)
             return redirect('/login')
         else:
             # sign up fail
@@ -51,15 +60,24 @@ def signup():
 
     return render_template('signup.html')
 
+
 @app.route('/home')
 def home():
+    '''
+    shows user's name and student management system
+    '''
     user = request.args.get('user')
     print(user)
     return render_template('manage_panel.html', user=user)
 
+
 @app.route('/forgot')
 def forgot():
+    '''
+    to find the account missed and reset password
+    '''
     return render_template('forgot.html')
+
 
 if __name__ == '__main__':
     app.run()
